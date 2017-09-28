9826a5eb0400ae5b4aecf48d58076a6f4ec68089
Leonardo Soares <leonardo.soares.ws@gmail.com>
2017-09-27 21:14:09 -0300

    component inside component 1

---
+++ ex/member.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default props => (
+    <div>
+        {props.name}&nbsp;
+        <strong>{props.lastName}</strong>
+    </div>
+)
---
+++ ex/silvaFamily.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Member from './member'
+
+export default () => (
+    <div>
+        <Member name='João' lastName='silva' />
+        <Member name='Maria' lastName='silva' /> 
+        <Member name='José' lastName='silva' /> 
+        <Member name='Pedro' lastName='silva' /> 
+    </div>
+)
