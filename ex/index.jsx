eef1820fd865371d035e14156ca2f549a9d4c7fe
Leonardo Soares <leonardo.soares.ws@gmail.com>
2017-10-01 19:28:45 -0300

    handle Change, controled and uncontroled component

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ClassComponent from './classComponent'
+import Field from './field'
 
 ReactDOM.render(
-    <ClassComponent label='Contador' initialValue={10} />
+    <Field label='Contador' initalValue="Olá" />
     , document.getElementById('app')
 )
